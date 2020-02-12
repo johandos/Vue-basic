9514ddfb9067b1a30583b39982f1f7d5537823ee
johandos <johan.ospina05@gmail.com>
2020-02-12 09:56:08 -0500

    computed - watch

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="app">
-    <h1>Proyecto VUE</h1>
-    <p>Un Parrafo</p>
-    <app-directivas></app-directivas>
-    <input type="text" v-power-mode>
+<!--    <h1>Proyecto VUE</h1>-->
+<!--    <p>Un Parrafo</p>-->
+<!--    <app-directivas></app-directivas>-->
+<!--    <input type="text" v-power-mode>-->
+    <app-computed-watch></app-computed-watch>
   </div>
 </template>
 <script>
-  import Directivas from './components/Directivas'
-
+  //import Directivas from './components/Directivas'
+  import ComputedWatch from './components/ComputedWatch'
   export default {
     name: 'App',
     components: {
-      appDirectivas: Directivas
+      /*appDirectivas: Directivas*/
+      appComputedWatch: ComputedWatch,
     }
   }
 </script>
---
+++ src/components/ComputedWatch.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <p :style="{backgroundColor: colorLetra}"> Nombre y apellido: {{nameAll}}</p>
+        <input type="text" v-model="name">
+        <input type="text" v-model="lastName">
+        <input type="text" v-model="city">
+        <p>Validacion: {{status}}</p>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                name: 'Johan David',
+                lastName: 'Ospina Alzate',
+                city: 'Madrid',
+                status: '....',
+                colorLetra: 'blue'
+            }
+        },
+        computed:{
+            nameAll(){
+                console.log('Se esta Modificando Nombre o Aellido');
+                return this.name + ' ' + this.lastName;
+            }
+        },
+        watch:{
+            name(newName, oldName){
+                setTimeout(() => {
+                    if (['tony', 'robo'].includes(newName.toLowerCase())){
+                        this.status = 'Nombre valido'
+                    } else {
+                        this.status = 'Nombre no valido'
+                    }
+                }, 500);
+
+                if (newName > oldName){
+                    this.colorLetra = 'yellowgreen'
+                }else{
+                    this.colorLetra = 'lightblue'
+                }
+            }
+        }
+    }
+</script>
